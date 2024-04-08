8c19a32c279950eb2244428abbbc23d190db010d
William Woodard <williamwoodard98@gmail.com>
2024-04-08 11:22:23 -0500

    chapter 11

--- src/components/employees/EmployeeDetails.jsx
+++ src/components/employees/EmployeeDetails.jsx
@@ -1,11 +1,43 @@
+import "./Employee.css"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { getEmployeeById } from "../../services/employeeDetailsService.jsx"
 
 export const EmployeeDetails = () => {
+    const [employee, setEmployee] = useState({})
     
-
     const { employeeId } = useParams()
 
+    useEffect(() => {
+        getEmployeeById(employeeId).then((data) => {
+            const employeeObj = data[0]
+            setEmployee(employeeObj)
+        })
+    }, [employeeId])
+
+    const employeeTicketCounter = () => {
+        return employee.employeeTickets?.length
+    }
+
+
     return (
-        {employeeId}
+        <section className="employee">
+        <header className="employee-header">{employee.user?.fullName}</header>
+        <div>
+            <span className="employee-info">Email :</span>
+            {employee.user?.email}
+        </div>
+        <div>
+            <span className="employee-info">Specialty :</span>
+            {employee.specialty}
+        </div>
+        <div>
+            <span className="employee-info">Rate : </span>
+            ${employee.rate}
+        </div>
+        <div>
+            Currently working on {employeeTicketCounter()} ticket(s)
+        </div>
+    </section>
     )
 }
